dbed9e0977f4a2785435b79615ec1fada130123d
midori yoridori <yoridrill@gmail.com>
2018-07-27 01:53:10 +0900

    複数マーカ対応／a-sky対応／リファクタ

--- js/index.js
+++ js/index.js
@@ -4,7 +4,7 @@ var webArViewer = webArViewer || {};
     webArViewer.scene = document.querySelector('a-scene');
     var ar = {
         C : {
-            arNum: 4
+            arNum: 5
         },
         init : function() {
             this.setArg();
@@ -38,13 +38,14 @@ var webArViewer = webArViewer || {};
             var self = this;
 
             var assets = document.createElement('a-assets');
+            assets.setAttribute('timeout', '9000');
             var arData = new Array(self.C.arNum+1).join('0').split('');
 
             if (!arData.some(function(val, idx) {
                 return self.arg['i' + idx];
             })) {
                 // 画像一つもなかった
-                if (window.confirm('画像情報が1つも取得できませんでした。\nジェネレータで再度QRコードを発行し直してください。')) {
+                if (window.confirm('画像情報が1つも取得できませんでした。\nジェネレータで再度作り直してください。')) {
                     location.href = "https://web-ar-generator.firebaseapp.com/";
                 }
                 return false;
@@ -68,7 +69,7 @@ var webArViewer = webArViewer || {};
                     arData[idx].size = {
                         w: arData[idx].size.w * 10,
                         h: arData[idx].size.h * 10
-                    }
+                    };
                 }
                 arData[idx].isGif = !!(self.arg['i' + idx]||'').match(/\.gif$/i);
 
@@ -79,9 +80,10 @@ var webArViewer = webArViewer || {};
                     source.setAttribute('id', 'source' + idx);
                     source.setAttribute('src', self.arg['i' + idx]);
                     assets.appendChild(source);
-                    webArViewer.scene.appendChild(assets);
                 }
             });
+
+            webArViewer.scene.appendChild(assets);
             self.arData = arData;
             return true;
         },
@@ -99,14 +101,16 @@ var webArViewer = webArViewer || {};
                 swHelp.classList.toggle('current');
             });
 
-            if(!self.arData) {
+            if(!self.arData  || location.pathname.match(/vr/)) {
                 return false;
             }
 
             if (self.arg.preview) {
                 swPreview.classList.add('current');
+                self.arg.multi = false;
             } else if (self.arg.gyro) {
                 swGyro.classList.add('current');
+                self.arg.multi = false;
             } else {
                 swMarker.classList.add('current');
             }
@@ -130,7 +134,11 @@ var webArViewer = webArViewer || {};
         setWrap : function() {
             var self = this;
 
-            if (self.arg.preview) {
+            if (location.pathname.match(/vr/)) {
+                var wrap = document.createElement('a-entity');
+                var vrPos = self.arg.vrPos ? decodeURI(self.arg.vrPos) : '0 0 -4';
+                wrap.setAttribute('position', vrPos);
+            } else if (self.arg.preview) {
                 var wrap = document.createElement('a-entity');
                 wrap.setAttribute('position', '0 0 -15');
                 wrap.setAttribute('rotation', '25 0 0');
@@ -163,7 +171,7 @@ var webArViewer = webArViewer || {};
                         zoomRate += (event.pageY - prevPageY) / webArViewer.scene.clientHeight / 5;
 
                         AFRAME.utils.entity.setComponentProperty(wrap, 'animation__scale', {
-                            property: 'scale', dur: 400, easing: 'easeInOutQuad', loop: false, to: zoomRate + ' ' + zoomRate + ' ' + zoomRate
+                            property: 'scale', dur: 5, easing: 'linear', loop: false, to: zoomRate + ' ' + zoomRate + ' ' + zoomRate
                         });
                     }
                 });
@@ -176,11 +184,13 @@ var webArViewer = webArViewer || {};
 
                 var camera = document.querySelector('a-camera-static');
                 camera.setAttribute('look-controls', 'look-controls');
+            } else if (self.arg.multi) {
+                return;
             } else {
                 var wrap = document.createElement('a-marker');
                 wrap.setAttribute('preset', 'custom');
                 wrap.setAttribute('type', 'pattern');
-                wrap.setAttribute('url', 'asset/pattern-marker.patt');
+                wrap.setAttribute('url', 'asset/ar0.patt');
             }
             self.wrap = wrap;
         },
@@ -188,106 +198,121 @@ var webArViewer = webArViewer || {};
             var self = this;
 
             self.arData.forEach(function(val, idx) {
-                if (val.path) {
-                    var h1_2 = val.size.h/2;
+                if (!val.path) {
+                    return;
+                }
 
-                    if (idx && val.isShadow) {
-                        var shadow = document.createElement('a-entity');
-
-                        var pos = [
-                            {x: 0, y: 0, z: 0},
-                            {x: 0, y: -0.2, z: - self.arData[0].size.h/2 - h1_2},
-                            {x: 0, y: -0.1, z: - h1_2},
-                            {x: 0, y: 0, z: self.arData[0].size.h/2 - h1_2}
-                        ];
-                        shadow.setAttribute('position', AFRAME.utils.coordinates.stringify(pos[idx]));
-                        shadow.setAttribute('rotation', '-90 0 0');
-
-                        AFRAME.utils.entity.setComponentProperty(shadow, 'geometry', {
-                            primitive: 'plane', height: val.size.h, width: val.size.w
-                        });
-                        AFRAME.utils.entity.setComponentProperty(shadow, 'material', {
-                            shader: val.isGif ? 'gif' : 'standard', npot: true, src: '#source' + idx, transparent: true, alphaTest: 0.1,
-                            color: 'black', opacity: 0.3
-                        });
-
-                        // アニメーション
-                        if (val.isPoyo) {
-                            AFRAME.utils.entity.setComponentProperty(shadow, 'animation__alpha', {
-                                property: 'material.opacity', dir: 'alternate', dur: 400, easing: 'easeInOutQuart', loop: true, to: '0.1'
-                            });
-
-                            AFRAME.utils.entity.setComponentProperty(shadow, 'animation__scale', {
-                                property: 'scale', dir: 'alternate', dur: 400, easing: 'easeInOutQuart', loop: true, to: '0.8 0.7 1'
-                            });
-                        }
-                        self.arData[idx].shadow = shadow;
-                    }
-
-                    var main = document.createElement('a-entity');
-
-                    var pos = [
-                        {x: 0, y: val.isWarp ? h1_2 : -0.2, z: 0},
-                        {x: 0, y: h1_2, z: (val.isWarp ? val.size.w : 0) - self.arData[0].size.h/2 - 0.1},
-                        {x: 0, y: h1_2, z: - (val.isWarp ? val.size.w : 0)},
-                        {x: 0, y: h1_2, z: self.arData[0].size.h/2 - (val.isWarp ? val.size.w : 0) + 0.1}
-                    ];
-
-                    main.setAttribute('position', AFRAME.utils.coordinates.stringify(pos[idx]));
-                    main.setAttribute('rotation', ((idx === 0 && !val.isWarp) ? -90 : 0) + ' 0 0');
-
-                    AFRAME.utils.entity.setComponentProperty(main, 'material', {
-                        shader: val.isGif ? 'gif' : 'standard', npot: true, src: '#source' + idx,
-                        side: 'double', transparent: true, alphaTest: 0.1
+                if (idx === 4) {
+                    var sky = document.createElement('a-sky');
+                    AFRAME.utils.entity.setComponentProperty(sky, 'material', {
+                        shader: val.isGif ? 'gif' : 'standard', src: '#source' + idx
                     });
-
-                    if (!val.isWarp) {
-                        AFRAME.utils.entity.setComponentProperty(main, 'geometry', {
-                            primitive: 'plane', height: val.size.h, width: val.size.w
-                        });
-                    } else if (idx) {
-                        var ts = [0, 212, -32, -32];
-                        AFRAME.utils.entity.setComponentProperty(main, 'geometry', {
-                            primitive: 'cylinder', openEnded: true, thetaStart: ts[idx], thetaLength: idx === 1 ? -64 : 64,
-                            height: val.size.h, radius: val.size.w
-                        });
-                    } else {
-                        AFRAME.utils.entity.setComponentProperty(main, 'geometry', {
-                            primitive: 'sphere', radius: h1_2, phiStart: -90
+                    if (val.isShadow) {
+                        AFRAME.utils.entity.setComponentProperty(sky, 'animation__rot', {
+                            property: 'geometry.phiStart', dur: 20000, easing: 'linear', loop: true, to: -360
                         });
                     }
+                    webArViewer.scene.appendChild(sky);
+                    return;
+                }
+
+                if (idx && val.isShadow) {
+                    var shadow = document.createElement('a-entity');
+
+                    shadow.setAttribute('position', AFRAME.utils.coordinates.stringify(self.positionVec3(idx, 'shadow')));
+                    shadow.setAttribute('rotation', '-90 0 0');
+
+                    AFRAME.utils.entity.setComponentProperty(shadow, 'geometry', {
+                        primitive: 'plane', height: val.size.h, width: val.size.w
+                    });
+                    AFRAME.utils.entity.setComponentProperty(shadow, 'material', {
+                        shader: val.isGif ? 'gif' : 'flat', npot: true, src: '#source' + idx, transparent: true, alphaTest: 0.1,
+                        color: 'black', opacity: 0.3
+                    });
 
                     // アニメーション
                     if (val.isPoyo) {
-                        AFRAME.utils.entity.setComponentProperty(main, 'animation__pos', {
-                            property: 'position', dir: 'alternate', dur: 400, easing: 'easeInOutQuart', loop: true, to: pos[idx].x + ' ' + (pos[idx].y+val.size.h/3) + ' ' + pos[idx].z
+                        AFRAME.utils.entity.setComponentProperty(shadow, 'animation__alpha', {
+                            property: 'material.opacity', dir: 'alternate', dur: 400, easing: 'easeInOutQuart', loop: true, to: '0.1'
                         });
 
-                        AFRAME.utils.entity.setComponentProperty(main, 'animation__scale', {
-                            property: 'scale', dir: 'alternate', dur: 400, easing: 'easeOutQuad', loop: true, to: '0.94 1.06 1'
-                        });
-
-                    }
-                    if (idx === 0 && val.isShadow) {
-                        AFRAME.utils.entity.setComponentProperty(main, 'animation__rot', {
-                            property: 'rotation', dur: 20000, easing: 'linear', loop: true, to: (val.isWarp ? 0 : -90) + ' 360 0'
+                        AFRAME.utils.entity.setComponentProperty(shadow, 'animation__scale', {
+                            property: 'scale', dir: 'alternate', dur: 400, easing: 'easeInOutQuart', loop: true, to: '0.8 0.7 1'
                         });
                     }
-                    self.arData[idx].main = main;
+                    self.arData[idx].shadow = shadow;
                 }
+
+                var main = document.createElement('a-entity');
+                var posVec3 = self.positionVec3(idx, 'main');
+
+                main.setAttribute('position', AFRAME.utils.coordinates.stringify(posVec3));
+                main.setAttribute('rotation', ((idx === 0 && !val.isWarp) ? -90 : 0) + ' 0 0');
+
+                AFRAME.utils.entity.setComponentProperty(main, 'material', {
+                    shader: val.isGif ? 'gif' : 'standard', npot: true, src: '#source' + idx,
+                    side: 'double', transparent: true, alphaTest: 0.1
+                });
+
+                if (!val.isWarp) {
+                    AFRAME.utils.entity.setComponentProperty(main, 'geometry', {
+                        primitive: 'plane', height: val.size.h, width: val.size.w
+                    });
+                } else if (idx) {
+                    var ts, tl;
+                    if (idx === 1 && !self.arg.multi) {
+                        ts = 212;
+                        tl = -64;
+                    } else {
+                        ts = -32;
+                        tl = 64;
+                    }
+                    AFRAME.utils.entity.setComponentProperty(main, 'geometry', {
+                        primitive: 'cylinder', openEnded: true, thetaStart: ts, thetaLength: tl,
+                        height: val.size.h, radius: val.size.w
+                    });
+                } else {
+                    AFRAME.utils.entity.setComponentProperty(main, 'geometry', {
+                        primitive: 'sphere', radius: val.size.w/2, phiStart: -90
+                    });
+                }
+
+                // アニメーション
+                if (val.isPoyo) {
+                    AFRAME.utils.entity.setComponentProperty(main, 'animation__pos', {
+                        property: 'position', dir: 'alternate', dur: 400, easing: 'easeInOutQuart', loop: true, to: posVec3.x + ' ' + (posVec3.y+val.size.h/3) + ' ' + posVec3.z
+                    });
+
+                    AFRAME.utils.entity.setComponentProperty(main, 'animation__scale', {
+                        property: 'scale', dir: 'alternate', dur: 400, easing: 'easeOutQuad', loop: true, to: '0.94 1.06 1'
+                    });
+
+                }
+                if (idx === 0 && val.isShadow) {
+                    AFRAME.utils.entity.setComponentProperty(main, 'animation__rot', {
+                        property: 'rotation', dur: 20000, easing: 'linear', loop: true, to: (val.isWarp ? 0 : -90) + ' 360 0'
+                    });
+                }
+                self.arData[idx].main = main;
             });
 
-            if(self.arData[0].isWarp) {
-                self.arData[1].shadow && self.wrap.appendChild(self.arData[1].shadow);
-                self.arData[2].shadow && self.wrap.appendChild(self.arData[2].shadow);
-                self.arData[3].shadow && self.wrap.appendChild(self.arData[3].shadow);
+            if (self.arg.multi) {
+                self.arData.forEach(function(val, idx) {
+                    if (!val.path || idx === 4) {
+                        return;
+                    }
+                    var arMarker = document.createElement('a-marker');
+                    arMarker.setAttribute('preset', 'custom');
+                    arMarker.setAttribute('type', 'pattern');
+                    arMarker.setAttribute('url', 'asset/ar' + idx + '.patt');
 
-                self.arData[1].main && self.wrap.appendChild(self.arData[1].main);
-                self.arData[2].main && self.wrap.appendChild(self.arData[2].main);
-                self.arData[0].main && self.wrap.appendChild(self.arData[0].main);
-                self.arData[3].main && self.wrap.appendChild(self.arData[3].main);
+                    self.arData[idx].shadow && arMarker.appendChild(self.arData[idx].shadow);
+                    self.arData[idx].main && arMarker.appendChild(self.arData[idx].main);
+
+                    webArViewer.scene.appendChild(arMarker);
+                });
             } else {
-                self.arData[0].main && self.wrap.appendChild(self.arData[0].main);
+                self.arData[0].main && !self.arData[0].isWarp && self.wrap.appendChild(self.arData[0].main);
 
                 self.arData[1].shadow && self.wrap.appendChild(self.arData[1].shadow);
                 self.arData[2].shadow && self.wrap.appendChild(self.arData[2].shadow);
@@ -295,19 +320,67 @@ var webArViewer = webArViewer || {};
 
                 self.arData[1].main && self.wrap.appendChild(self.arData[1].main);
                 self.arData[2].main && self.wrap.appendChild(self.arData[2].main);
+                self.arData[0].main && self.arData[0].isWarp && self.wrap.appendChild(self.arData[0].main);
                 self.arData[3].main && self.wrap.appendChild(self.arData[3].main);
+
+                webArViewer.scene.appendChild(self.wrap);
             }
-            webArViewer.scene.appendChild(self.wrap);
+        },
+        // type: ['shadow' | 'main']
+        positionVec3: function (idx, type) {
+            var self = this;
+            var h1_2 = self.arData[idx].size.h/2;
+            var width = self.arData[idx].size.w;
+            var isWarp = self.arData[idx].isWarp;
+
+            if (type === 'shadow') {
+                if (self.arg.multi) {
+                    return {x: 0, y: -0.1, z: - h1_2};
+                } else {
+                    var p = {};
+                    p[1] = function () {
+                        return {x: 0, y: -0.2, z: - self.arData[0].size.h/2 - h1_2};
+                    };
+                    p[2] = function () {
+                        return {x: 0, y: -0.1, z: - h1_2};
+                    };
+                    p[3] = function () {
+                        return {x: 0, y: 0, z: self.arData[0].size.h/2 - h1_2};
+                    };
+                    return p[idx]();
+                }
+            } else {
+                if (idx === 0) {
+                    return {x: 0, y: isWarp ? width/2 : -0.2, z: 0};
+                } else if (self.arg.multi) {
+                    return {x: 0, y: h1_2, z: - (isWarp ? width : 0)};
+                } else {
+                    var p = {};
+                    p[1] = function () {
+                        return {x: 0, y: h1_2, z: (isWarp ? width : 0) - self.arData[0].size.h/2 - 0.1};
+                    };
+                    p[2] = function () {
+                        return {x: 0, y: h1_2, z: - (isWarp ? width : 0)};
+                    };
+                    p[3] = function () {
+                        return {x: 0, y: h1_2, z: - (isWarp ? width : 0) + self.arData[0].size.h/2 + 0.1};
+                    };
+                    return p[idx]();
+                }
+            }
         }
     };
 
     var vr = {
-        cEle : document.getElementById('rightlens'),
+        cEle : null,
         videoDom : null,
         rLensTimer : null,
 
         init : function() {
-            this.setEvents();
+            this.cEle = document.getElementById('rightlens');
+            if (this.cEle) {
+                this.setEvents();
+            }
         },
         setEvents : function() {
             var self = this;
